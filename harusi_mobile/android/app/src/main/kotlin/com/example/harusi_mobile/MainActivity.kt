1b37da7ef151936d8eff5aeefc89c5f9aa0226ad
salva66 <henrymkama@gmail.com>
2025-11-20 17:21:40 +0300

    begin_android_app

---
+++ harusi_mobile/android/app/src/main/kotlin/com/example/harusi_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.harusi_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ harusi_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import path_provider_foundation
+import rive_native
+import shared_preferences_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RiveNativePlugin.register(with: registry.registrar(forPlugin: "RiveNativePlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+}
